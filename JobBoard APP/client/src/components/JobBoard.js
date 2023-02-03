109b72bcb3a1daa04558ad7d256809c9b45de74f
Junaidhkn <junaidhkniazi@gmail.com>
2023-02-04 01:04:37 +0500

    commit

--- JobBoard APP/client/src/components/JobBoard.js
+++ JobBoard APP/client/src/components/JobBoard.js
@@ -1,27 +1,8 @@
 import JobList from './JobList';
-import { JOBS_QUERY } from '../graphQL/queries.js';
-import { useQuery } from '@apollo/client';
 import { useJobs } from '../graphQL/hooks.js';
 
 
 function JobBoard () {
-
-
-  //  const JOBS_QUERY = gql`
-  //    query JobsQuery{
-  //   jobs {
-  //     id
-  //     title
-  //     description
-  //     company {
-  //       id
-  //       name
-  //     }
-  //   }
-  // }
-
-  // `
-
   const { jobs, loading, error } = useJobs()
 
   if ( loading ) {
--- JobBoard APP/client/src/graphQL/hooks.js
+++ JobBoard APP/client/src/graphQL/hooks.js
@@ -2,6 +2,22 @@ import { useQuery } from "@apollo/client"
 import { JOBS_QUERY } from "./queries.js"
 
 
+// Just For Query Reference
+
+//  const JOBS_QUERY = gql`
+//    query JobsQuery{
+//   jobs {
+//     id
+//     title
+//     description
+//     company {
+//       id
+//       name
+//     }
+//   }
+// }
+
+// `
 
 export const useJobs = () => {
    const { data, loading, error } = useQuery( JOBS_QUERY, {

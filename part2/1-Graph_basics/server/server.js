3b93efbbabdb97a5ee1a579aae1e83ec4a3f7de0
Junaidhkn <junaidhkniazi@gmail.com>
2023-04-28 23:17:35 +0500

    commit

--- part2/1-Graph_basics/server/server.js
+++ part2/1-Graph_basics/server/server.js
@@ -1,11 +1,16 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+
+// Type definitions define the "shape" of your data and specify what a client can request / Query
+
 const typeDefs = `#graphql
   type Query {
     greeting: String
   }
 `;
+
+// Resolvers define the technique for fetching the types defined, returns the actual value 
 
 const resolvers = {
   Query: {
@@ -13,6 +18,6 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
-console.log(`Server running at ${url}`);
+const server = new ApolloServer( { typeDefs, resolvers } );
+const { url } = await startStandaloneServer( server, { listen: { port: 9000 } } );
+console.log( `Server running at ${url}` );

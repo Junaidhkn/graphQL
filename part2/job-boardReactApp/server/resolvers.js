9635715a5d89a4820b7c2e912f450aaf8ee5c1e4
Junaidhkn <junaidhkniazi@gmail.com>
2023-05-11 21:35:54 +0500

    non- nullability

--- part2/job-boardReactApp/server/resolvers.js
+++ part2/job-boardReactApp/server/resolvers.js
@@ -1,11 +1,14 @@
 export const resolvers = {
    Query: {
-      job: () => {
-         return {
-            title: 'Software Engineer',
-            company: 'Google',
-            description: "Best job ever"
-         }
+      jobs: () => {
+         return [
+            {
+               id: '1',
+               title: 'Software Engineer',
+               company: 'Google',
+               description: "Best job ever"
+            }
+         ]
       }
    }
 }

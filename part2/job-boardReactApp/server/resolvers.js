2301dd56a47bf906c4d9254be8c8601dc29d2e46
Junaidhkn <junaidhkniazi@gmail.com>
2023-05-13 16:44:23 +0500

    date in GraphQL

--- part2/job-boardReactApp/server/resolvers.js
+++ part2/job-boardReactApp/server/resolvers.js
@@ -2,8 +2,12 @@ import { getJobs } from "./db/jobs.js"
 
 export const resolvers = {
    Query: {
-      jobs: async () => {
-         return await getJobs()
+      jobs: () => getJobs()
+   },
+   Job: {
+      date: ( job ) => {
+         return job.createdAt.toISOString().slice( 0, 'yyyy-mm-dd'.length )
       }
    }
+
 }

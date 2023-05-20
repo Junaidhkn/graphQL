aed5e9f9aa1703b26279ac30856f594ec497f515
Junaidhkn <junaidhkniazi@gmail.com>
2023-05-21 01:45:19 +0500

    commit

--- part2/job-boardReactApp/client/src/pages/CompanyPage.js
+++ part2/job-boardReactApp/client/src/pages/CompanyPage.js
@@ -5,14 +5,31 @@ import JobList from '../components/JobList.js';
 
 function CompanyPage () {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  const [state, setState] = useState( {
+    company: null,
+    loading: true,
+    error: false
+  } );
   useEffect( () => {
-    getCompany( companyId ).then( companyById => {
-      setCompany( companyById )
-    } )
-  }, [companyId, setCompany] )
+    ( async () => {
+      try {
+        const company = await getCompany( companyId );
+        setState( { company: company, loading: false, error: false } );
+      } catch {
+        setState( { company: null, loading: false, error: true } );
+      }
+    } )();
+  }, [companyId] )
 
-  if ( !company ) return <h1>Loading...</h1>
+  const { company, loading, error } = state;
+
+  if ( loading ) {
+    return <div>Loading...</div>
+  }
+
+  if ( error ) {
+    return <div className='has-text-danger'>Data requested is unavailable!!!!</div>
+  }
   return (
     <div>
       <h1 className="title">

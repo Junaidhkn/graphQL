be4a0c01acea63f309cca529f9f78c253c463e04
junaidhkn <junaidhkniazi@gmail.com>
2023-05-31 23:09:39 +0500

    commit

--- part2/job-boardReactApp/server/resolvers.js
+++ part2/job-boardReactApp/server/resolvers.js
@@ -1,6 +1,6 @@
 import { GraphQLError } from 'graphql';
 
-import { createJob, getJob, getJobs, getJobsByCompany } from "./db/jobs.js"
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany } from "./db/jobs.js"
 import { getCompany } from "./db/companies.js"
 
 export const resolvers = {
@@ -32,6 +32,9 @@ export const resolvers = {
          const companyId = 'FjcJCHJALA4i'
          const { title, description } = args.input
          return createJob( { companyId, title, description } )
+      },
+      deleteJob: ( _root, args ) => {
+         return deleteJob( args.id )
       }
    },
 

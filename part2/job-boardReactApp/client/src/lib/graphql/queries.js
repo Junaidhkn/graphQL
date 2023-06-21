0048015fd11154bc02e0359ebb989b71d33ef6db
junaidhkn <junaidhkniazi@gmail.com>
2023-06-21 21:58:33 +0500

    get Jobs using Apollo Client

--- part2/job-boardReactApp/client/src/lib/graphql/queries.js
+++ part2/job-boardReactApp/client/src/lib/graphql/queries.js
@@ -1,19 +1,23 @@
-import { GraphQLClient, gql } from 'graphql-request'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { getAccessToken } from '../auth.js';
 
 
-export const client = new GraphQLClient( 'http://localhost:9000/', {
-   headers: () => {
-      const accessToken = getAccessToken()
-      if ( accessToken ) {
-         return {
-            authorization: `Bearer ${accessToken}`
-         }
-      }
-      return {}
-   }
-} );
+// export const client = new GraphQLClient( 'http://localhost:9000/', {
+//    headers: () => {
+//       const accessToken = getAccessToken()
+//       if ( accessToken ) {
+//          return {
+//             authorization: `Bearer ${accessToken}`
+//          }
+//       }
+//       return {}
+//    }
+// } );
 
+export const client = new ApolloClient( {
+   uri: 'http://localhost:9000/',
+   cache: new InMemoryCache()
+} )
 
 export const getJobs = async () => {
    const query = gql`
@@ -29,44 +33,44 @@ export const getJobs = async () => {
       }
    }
    `
-   const { jobs } = await client.request( query );
-   return jobs;
+   const { data } = await client.query( { query } )
+   return data.jobs;
 }
 
 
-export const getJob = async ( id ) => {
-   const query = gql`
-   query JobById ($id:ID!){
-      job(id:$id) {
-         date
-         title
-         description
-      company {
-         name
-         id
-    }
-  }
-   }
-   `
-   const { job } = await client.request( query, { id } );
-   return job;
-}
+// export const getJob = async ( id ) => {
+//    const query = gql`
+//    query JobById ($id:ID!){
+//       job(id:$id) {
+//          date
+//          title
+//          description
+//       company {
+//          name
+//          id
+//     }
+//   }
+//    }
+//    `
+//    const { job } = await client.request( query, { id } );
+//    return job;
+// }
 
-export const getCompany = async ( id ) => {
-   const query = gql`
-   query ComapanyById ($id:ID!){
-      company(id:$id) {
-         id
-         name
-         description
-         jobs{
-            id
-            date
-            title
-         }
-  }
-   }
-   `
-   const { company } = await client.request( query, { id } );
-   return company;
-}
+// export const getCompany = async ( id ) => {
+//    const query = gql`
+//    query ComapanyById ($id:ID!){
+//       company(id:$id) {
+//          id
+//          name
+//          description
+//          jobs{
+//             id
+//             date
+//             title
+//          }
+//   }
+//    }
+//    `
+//    const { company } = await client.request( query, { id } );
+//    return company;
+// }

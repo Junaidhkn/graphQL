730ab54afaec54c421faa36e695fd8d77cccdeec
junaidhkn <junaidhkniazi@gmail.com>
2023-06-23 17:11:43 +0500

    apollo Provider Class instance

--- part2/job-boardReactApp/client/src/App.js
+++ part2/job-boardReactApp/client/src/App.js
@@ -1,3 +1,4 @@
+import { ApolloProvider } from '@apollo/client';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Route, Routes } from 'react-router-dom';
@@ -8,23 +9,24 @@ import CreateJobPage from './pages/CreateJobPage';
 import HomePage from './pages/HomePage';
 import JobPage from './pages/JobPage';
 import LoginPage from './pages/LoginPage';
+import { client } from './lib/graphql/queries.js';
 
-function App() {
+function App () {
   const navigate = useNavigate();
-  const [user, setUser] = useState(getUser);
+  const [user, setUser] = useState( getUser );
 
-  const handleLogin = (user) => {
-    setUser(user);
-    navigate('/');
+  const handleLogin = ( user ) => {
+    setUser( user );
+    navigate( '/' );
   };
 
   const handleLogout = () => {
-    setUser(null);
-    navigate('/');
+    setUser( null );
+    navigate( '/' );
   };
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <NavBar user={user} onLogout={handleLogout} />
       <main className="section">
         <Routes>
@@ -45,7 +47,7 @@ function App() {
           />
         </Routes>
       </main>
-    </>
+    </ApolloProvider>
   );
 }
 

d02fe3b492052ccabde61d4cf9943872221f7eeb
junaidhkn <junaidhkniazi@gmail.com>
2023-06-29 00:54:57 +0500

    client side pagination

--- part2/job-boardReactApp/client/src/pages/HomePage.js
+++ part2/job-boardReactApp/client/src/pages/HomePage.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import JobList from '../components/JobList';
 import { useJobs } from '../lib/graphql/hooks.js';
 
 function HomePage () {
+  const [currentPage, setCurrrentPage] = useState( 1 )
 
-  const { jobs, loading, error } = useJobs()
+  const limit = 5
+  const offset = ( currentPage - 1 ) * limit
+  const { jobs, loading, error } = useJobs( limit, offset )
 
   if ( loading ) {
     return <div>Loading...</div>
@@ -13,12 +17,28 @@ function HomePage () {
     return <div className='has-text-danger'>Data requested is unavailable!!!!</div>
   }
 
+  const nextPage = () => {
+    setCurrrentPage( currentPage + 1 )
+  }
+  const previousPage = () => {
+    if ( currentPage <= 1 ) {
+      return
+    }
+    setCurrrentPage( currentPage - 1 )
+  }
+
   return (
     <div>
       <h1 className="title">
         Job Board
       </h1>
       <JobList jobs={jobs} />
+
+      <div>
+        <button className="button is-primary" onClick={previousPage}>Load Less</button>
+        <span>{currentPage}</span>
+        <button className="button is-primary" onClick={nextPage}>Load More</button>
+      </div>
     </div>
   );
 }
